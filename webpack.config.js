4bde791ae65cb18e8e2d47c012009cbcf9676fb2
shibaiyong <1850418798@qq.com>
2017-11-13 23:29:26 +0800

    shi

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,12 @@ module.exports={
 	entry:__dirname+"/webpack-app/index.js",
 	output: {
 		path:__dirname+"/webpack-public",
-		filename:'bundle.js'
-	}
+		filename:'bundle[hash].js'
+	},
+	devtool: 'eval-source-map',
+	devServer: {
+    contentBase: "./webpack-public",//本地服务器所加载的页面所在的目录
+    historyApiFallback: true,//不跳转
+    inline: true//实时刷新
+  } 
 }

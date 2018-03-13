724ba84e8fe072a092238d75b0c0641201f40ce5
lijiaxu <MuZi_Nine@163.com>
2018-03-13 21:32:53 +0800

    shibaiyong change

--- react/flux/src/scripts/components/ContentComponent.js
+++ react/flux/src/scripts/components/ContentComponent.js
@@ -1,10 +1,14 @@
+//主要内容：ref在以后的发展中可能需要通过函数的方式去使用ref
 var React = require('react')
 
 var ContentComponent = React.createClass({
+    handleClick:function(){
+        this.div.style.background="blue"
+    },
 	render:function(){
 		return (
 			<div className="content">
-                <div className="content-block">这里是content</div>
+                <div onClick={this.handleClick} className="content-block" ref={(div)=>{this.div=div}}>这里是content</div>
             </div>
 		)
 	}
